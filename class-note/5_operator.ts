d422e1d3da3e18b71003db09bc418151743dd5ae
greenT-Hee <heehee970322@gmail.com>
2022-09-16 21:25:57 +0900

    Add:유니온과 인터섹션

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -1,5 +1,5 @@
 // 유니온 타입 : 2가지 이상 타입 사용 가능
-let taetae : string | number
+let taetae : string | number | boolean
 function logMessage(value: string | number) {
     // 타입 가드
     if(typeof value === 'number') {
@@ -29,14 +29,34 @@ interface Developerrr {
 
 interface Personnnnn {
     name: string;
-    age: string;
+    age: number;
 }
 
+// 유니온 타입으로 호출
 function askSomeone (someone: Developerrr | Personnnnn) {
     // 공통 속성만 가능
-    someone.name
+    // someone.name
+
+
+    // age, skill을 사용하고 싶으면 type guide를 사용해야 한다.
+}
+askSomeone({name : '디벨로퍼',skill : '웹 개발'})
+askSomeone({name : '태희', age : 100})
+
+// intersection type (인터섹션 타입)
+let hee : string | number | boolean;
+let cat : string & number & boolean;
+
+// 인터섹션으로 호출 
+function askSomeone1 (someone: Developerrr & Personnnnn) {
+    // someone.name
     // someone.age
     // someone.skill
 
     // age, skill을 사용하고 싶으면 type guide를 사용해야 한다.
 }
+askSomeone1({name : '디벨로퍼',skill : '웹 개발', age: 200})
+
+
+// 실무에서는 union 타입을 더 자주 사용
+
