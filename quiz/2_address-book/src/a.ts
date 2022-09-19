b47a57fe2aa8142bdc834daa4def0dc239bfb9a0
greenT-Hee <heehee970322@gmail.com>
2022-09-19 23:57:47 +0900

    Add:fetch에 promise와 제너릭

---
+++ quiz/2_address-book/src/a.ts
@@ -0,0 +1,8 @@
+function fetchItems() : Promise<string[]> {
+    let items : string [] = ["a", 'b', 'c'];
+    return new Promise (function (resolve){
+        resolve(items);
+    })
+}
+
+fetchItems()
